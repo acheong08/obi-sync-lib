4a59d3358673501e486643fa5da2a5b73718cef7
Antonio Cheong <acheong@student.dalat.org>
2023-09-23 11:10:25 +0100

    async causes issues if not in the same block

--- tests/vault_test.ts
+++ tests/vault_test.ts
@@ -1,17 +1,18 @@
 import { ObiSync } from "../src/";
 import { MakeKeyHash } from "../src/crypt";
 
-MakeKeyHash("password", "salt").then((hash) => {
-  console.log("Hash:", hash);
-});
+// Create and run async block
+(async () => {
+  console.log(await MakeKeyHash("password", "salt"));
 
-const endpoint = process.env.OBI_ENDPOINT || "http://localhost:3000";
+  const endpoint = process.env.OBI_ENDPOINT || "http://localhost:3000";
 
-const sync = new ObiSync(endpoint);
-sync
-  .signin(process.env.OBI_EMAIL!, process.env.OBI_PASSWORD!)
-  .then((signedIn: boolean) => {
-    console.log("Signed in:", signedIn);
-  });
-
-console.log("DONE");
+  const sync = new ObiSync(endpoint);
+  console.log(
+    "Signed in:",
+    await sync.signin(process.env.OBI_EMAIL!, process.env.OBI_PASSWORD!)
+  );
+  console.log("Listing vaults...");
+  const vaults = await sync.list_vaults();
+  console.log("Vaults:", vaults);
+})();
